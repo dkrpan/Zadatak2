87ecda05524d30cc2b38224df541aeb5f4b42d7c
dkrpan <dom.krpan@gmail.com>
2018-01-14 22:32:00 +0100

    Initial commit

---
+++ magic.js
@@ -0,0 +1,92 @@
+$(document).ready(function() {
+
+    // process the form
+    $('form').submit(function(event) {
+
+
+        var formData = {
+            'name'              : $('input[name=name]').val(),
+            'email'             : $('input[name=email]').val(),
+            'contact'    : $('input[name=contact]').val()
+        };
+
+        // process the form
+        $.ajax({
+            type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+            url         : 'process.php', // the url where we want to POST
+            data        : formData, // our data object
+            dataType    : 'json', // what type of data do we expect back from the server
+                        encode          : true
+        })
+            // using the done promise callback
+            .done(function(data) {
+
+                // log data to the console so we can see
+                console.log(data); 
+
+                // here we will handle errors and validation messages
+            });
+
+        // stop the form from submitting the normal way and refreshing the page
+        event.preventDefault();
+    });
+
+});
+$.ajax({
+    type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+    url         : 'process.php', // the url where we want to POST
+    data        : formData, // our data object
+    dataType    : 'json' // what type of data do we expect back from the server
+})
+    // using the done promise callback
+    .done(function(data) {
+
+        // log data to the console so we can see
+        console.log(data);
+
+        //  errors and validation messages
+        if ( ! data.success) {
+
+            // handle errors for name
+            if (data.errors.name) {
+                $('#name-group').addClass('has-error'); // add the error class to show red input
+                $('#name-group').append('<div class="help-block">' + data.errors.name + '</div>'); // add the actual error message under our input
+            }
+
+            // handle errors for email
+            if (data.errors.email) {
+                $('#email-group').addClass('has-error'); // add the error class to show red input
+                $('#email-group').append('<div class="help-block">' + data.errors.email + '</div>'); // add the actual error message under our input
+            }
+
+            // handle errors for contact
+            if (data.errors.contactAlias) {
+                $('#contact-group').addClass('has-error'); // add the error class to show red input
+                $('#contact-group').append('<div class="help-block">' + data.errors.contactAlias + '</div>'); // add the actual error message under our input
+            }
+
+        } else {
+
+            // ALL GOOD! just show the success message!
+            $('form').append('<div class="alert alert-success">' + data.message + '</div>');
+
+            // usually after form submission, you'll want to redirect
+            // window.location = '/thank-you'; // redirect a user to another page
+            alert('success'); // for now we'll just alert the user
+
+        }
+
+    });
+	
+	$('form').submit(function(event) {
+
+    $('.form-group').removeClass('has-error'); // remove the error class
+    $('.help-block').remove(); // remove the error text
+	$.ajax({
+    type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+    url         : 'process.php', // the url where we want to POST
+    data        : formData, // our data object
+    dataType    : 'json' // what type of data do we expect back from the server
+})
+
+	
